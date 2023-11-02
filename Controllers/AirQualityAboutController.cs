7386462f004b9ae738a6a72980beea2a841e4c3d
Vandana Sirpa <sirpavandana@gmail.com>
2023-11-01 22:27:15 -0400

    Updated Changes

---
+++ Controllers/AirQualityAboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirQuality.Controllers
+{
+    public class AirQualityAboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/AirQualityDataModelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirQuality.Controllers
+{
+    public class AirQualityDataModelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
